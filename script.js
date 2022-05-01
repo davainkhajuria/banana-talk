4933f1307ed4e6684e31254ec4690ddce282ca8a
Davain Khajuria <81259338+davainkhajuria@users.noreply.github.com>
2022-05-01 12:47:49 +0530

    js update

--- script.js
+++ script.js
@@ -2,8 +2,24 @@ var txtInput = document.querySelector("#txt-input");
 var btnTranslate = document.querySelector("#btn-translate");
 var outputDiv = document.querySelector("#output");
 
-function clickHandler(){
-    outputDiv.innerText = "Banana translation of " + txtInput.value;
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
 }
+
+
+function clickHandler(){
+    var text = txtInput.value //taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(text))
+        .then(response => response.json())
+        .then(json =>{
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+}
+
 
 btnTranslate.addEventListener("click", clickHandler);
